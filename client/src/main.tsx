274afde4428077ff7fb423b9c0821f60cbbdaa69
yash <yashporwal208@gmail.com>
2024-06-25 13:27:00 +0530

    deploy

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,46 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom";
-import LoginPage from './components/LoginPage.tsx';
-import RegistrationPage from './components/RegistrationPage.tsx';
-import AddProductPage from './components/AddProductPage.tsx';
-import GeneratePDFPage from './components/GeneratePDFPage.tsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./components/LoginPage.tsx";
+import RegistrationPage from "./components/RegistrationPage.tsx";
+import AddProductPage from "./components/AddProductPage.tsx";
+import GeneratePDFPage from "./components/GeneratePDFPage.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: (
-     <LoginPage/>
-    ),
+    element: <LoginPage />,
   },
   {
     path: "/register",
-    element: (
-     <RegistrationPage/>
-    ),
+    element: <RegistrationPage />,
   },
   {
     path: "/add-product",
-    element: (
-     <AddProductPage/>
-    ),
+    element: <AddProductPage />,
   },
   {
     path: "/generate-pdf",
-    element: (
-     <GeneratePDFPage/>
-    ),
+    element: <GeneratePDFPage />,
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
